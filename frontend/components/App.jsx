40d97b539446cf44f6052a1461f9d8ef60e16bb5
Don Sirivat <don@joinhandshake.com>
2019-07-25 16:51:02 -0700

    Setup initial session form components

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route} from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import LoginFormContainer from './session_form/login_form_container';
 
 const App = () => (
   <div>
     <h1>Unify - The college marketplace</h1>
     <GreetingContainer />
+
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 
---
+++ frontend/components/session_form/signup_form.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email_address: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e => this.setState({
+      [field]: e.currentTarget.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  renderErrors() {
+    return(
+      <ul>
+        {this.props.errors.map((error, i) => (
+          <li key={`error-${i}`}>
+            {error}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="signup-form-container">
+        <form onSubmit={this.handleSubmit} className="signup-form-box">
+          Welcome to Unify!
+          <br/>
+          Please Sign Up to continue or {this.props.navLink}
+          {this.renderErrors()}
+          <div className="signup-form">
+            <br/>
+            <label>Email Address:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('email_address')}
+                className="signup-input"
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="signup-input"
+              />
+            </label>
+            <br/>
+            <input className="signup-submit" type="submit" value="signup" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignupForm;
