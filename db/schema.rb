86142e74702be3a5f43889af41eaa6025b93aaf1
Don Sirivat <don@joinhandshake.com>
2019-07-24 22:12:03 -0700

    Rename column to email address

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_033757) do
+ActiveRecord::Schema.define(version: 2019_07_25_051117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.string "email_address", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.string "first_name"
@@ -24,8 +24,8 @@ ActiveRecord::Schema.define(version: 2019_07_20_033757) do
     t.integer "school_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
