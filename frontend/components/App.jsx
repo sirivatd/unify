04c35f21f26a35d92ad0c0678340fc2d15e0576f
Don Sirivat <don@joinhandshake.com>
2019-07-27 23:48:00 -0700

    Add initial styling to header and login/signup form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,13 +4,17 @@ import GreetingContainer from './greeting/greeting_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 
+import './styles.scss';
+
 const App = () => (
   <div>
-    <h1>Unify - The college marketplace</h1>
-    <GreetingContainer />
+    <div className="header-menu">
+      <img src="https://i.ibb.co/tYRyxH5/unify-logo-white.png" className="header-logo" />
+      <GreetingContainer />
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+      <Route path="/login" component={LoginFormContainer} />
+      <Route path="/signup" component={SignupFormContainer} />
+    </div>
   </div>
 );
 
--- frontend/components/session_form/login_form.jsx
+++ frontend/components/session_form/login_form.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles.scss';
+
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
