754deab029774e470074e32c5fa320bbe645e8f9
Don Sirivat <don@joinhandshake.com>
2019-07-28 00:03:57 -0700

    Create items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_25_051117) do
+ActiveRecord::Schema.define(version: 2019_07_28_065636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.text "description", null: false
+    t.float "price"
+    t.string "subtitle"
+    t.string "name", null: false
+    t.integer "user_id"
+    t.date "expiration_date"
+    t.string "condition"
+    t.integer "school_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["school_id"], name: "index_items_on_school_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email_address", null: false
