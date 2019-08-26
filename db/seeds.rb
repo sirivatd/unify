b9f2cc301e30158c8d5b5f2170c32108179f7403
Don Sirivat <don@joinhandshake.com>
2019-08-25 23:46:44 -0700

    Add category image url in db and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,14 +23,14 @@ user = User.create(email_address: "admin@unify.com", school_id: school.id, first
 user = User.create(email_address: "don@joinhandshake.com", school_id: school.id, first_name: "Don", last_name: "Sirivat", password: "password")
 # Create categories
 
-Category.create(name: "Electronics")
-Category.create(name: "Textbooks")
-Category.create(name: "Apparel")
-Category.create(name: "Tickets")
-Category.create(name: "Furniture")
-Category.create(name: "Beauty & Health")
-Category.create(name: "Services")
-Category.create(name: "Other")
+Category.create(name: "Electronics", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Felectronics_category.png?alt=media&token=5466136c-46ad-426b-aa7b-f5d191b64a05")
+Category.create(name: "Textbooks", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Ftextbooks_category.png?alt=media&token=b837df7b-ef7c-4db0-94cd-5a93ce41b461")
+Category.create(name: "Apparel", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Fapparel_category.png?alt=media&token=22a08df4-3f0e-4f42-873b-eaca6f4c7828")
+Category.create(name: "Tickets", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Ftickets_category.png?alt=media&token=e4ae9b76-c8b4-4e07-b20e-e372f7376ace")
+Category.create(name: "Furniture", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Ffurniture_category.png?alt=media&token=43cdd7aa-3744-47f3-be46-1b82181630c9")
+Category.create(name: "Beauty & Health", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Fbeauty_category.png?alt=media&token=76dc0e26-a255-4039-9cfd-f6b4d1c04069")
+Category.create(name: "Sports & Outdoor", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Fsports_category.png?alt=media&token=376209f0-a075-48f2-a32f-403ec5f16456")
+Category.create(name: "Other", image_url: "https://firebasestorage.googleapis.com/v0/b/unify-aaba7.appspot.com/o/images%2Fother_category.png?alt=media&token=538fc42a-3c8a-44ab-b385-b9bbde849643")
 
 # Create conditions
 
