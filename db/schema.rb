7514949f33a185900925745bd188695fc489ee75
Don Sirivat <don@joinhandshake.com>
2019-08-25 23:11:55 -0700

    Add category image url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_23_190604) do
+ActiveRecord::Schema.define(version: 2019_08_26_061110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_08_23_190604) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "conditions", force: :cascade do |t|
