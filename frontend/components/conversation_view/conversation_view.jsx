5da39b6581219f3b941a8d10a73b9663d2a3464d
Don Sirivat <don@joinhandshake.com>
2019-09-01 00:31:49 -0700

    Add conversations sidebar

--- frontend/components/conversation_view/conversation_view.jsx
+++ frontend/components/conversation_view/conversation_view.jsx
@@ -12,13 +12,25 @@ class ConversationView extends React.Component {
   }
 
   render() {
-    const { conversations } = this.props;
+    const { conversations, currentUser } = this.props;
 
     return (
       <div className="messages-content-section">
-        <div className="conversation-view-section">
-          This is where the conversations will go
-        </div>
+        <ul className="conversation-view-section">
+          <div className="conversation-view-wrapper">
+            {conversations.map(conversation => {
+              return (
+                <li className="conversation-section-row">
+                  <img className="conversation-row-img" src={conversation.item_image_url} />
+                  <div className="conversation-row-txt">
+                    <h2 className="conversation-row-name">{ parseInt(currentUser.id) === conversation.sender_id ? conversation.recipient_name : conversation.sender_name }</h2>
+                    <h4 className="conversation-row-date">{conversation.updated_at}</h4>
+                  </div>
+                </li>
+              )
+            })}
+          </div>
+        </ul>
         <div className="message-view-section">
           This is where the messages will go
         </div>
