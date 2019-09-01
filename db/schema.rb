63644b14dfe025241ee06a3adac982a6106c98e6
Don Sirivat <don@joinhandshake.com>
2019-08-31 20:06:00 -0700

    Create message and conversation models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_070538) do
+ActiveRecord::Schema.define(version: 2019_09_01_030533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2019_08_28_070538) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id", "sender_id"], name: "index_conversations_on_recipient_id_and_sender_id", unique: true
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
   create_table "favorites", force: :cascade do |t|
@@ -63,6 +74,16 @@ ActiveRecord::Schema.define(version: 2019_08_28_070538) do
     t.index ["school_id"], name: "index_items_on_school_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "conversation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "schools", force: :cascade do |t|
     t.string "motto"
     t.string "nickname"
@@ -97,4 +118,6 @@ ActiveRecord::Schema.define(version: 2019_08_28_070538) do
   add_foreign_key "items", "categories"
   add_foreign_key "items", "conditions"
   add_foreign_key "items", "schools"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
