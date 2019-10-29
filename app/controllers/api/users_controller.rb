47f1f9edb60e879fef34d9b85b6605c665adccff
Don Sirivat <don@joinhandshake.com>
2019-10-29 00:03:36 -0700

    Add Redlands email verification during signup

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,16 @@
 class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
-    @user.assign_attributes({ school_id: 1})
+
+    # Automatically assign user to Redlands which is the only school in the system
+    if valid_edu_email_address?(user_params[:email_address])
+      @user.assign_attributes({ school: School.first } ) 
+    else
+      @user.errors.add(:base, "Must use a valid Redlands email address to register")
+      render json: @user.errors.full_messages, status: 422
+      return
+    end
+
     if @user.save
       login(@user)
       render "api/users/show"
@@ -14,5 +23,10 @@ class Api::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email_address, :password, :first_name, :last_name)
+  end
+
+  def valid_edu_email_address?(email)
+    school_name = email.split("@")[1]
+    school_name&.downcase == "redlands.edu"
   end
 end
